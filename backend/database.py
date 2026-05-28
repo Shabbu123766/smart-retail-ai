86423837b4878889a506fed166bdc072d758e5f3
Shabbu Parveen <shabbuparveen377@gmail.com>
2026-05-28 23:03:42 +0530

    Fixed MongoDB SSL connection

--- backend/database.py
+++ backend/database.py
@@ -1,12 +1,17 @@
 from pymongo import MongoClient
 from dotenv import load_dotenv
+import certifi
 import os
 
 load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
 
-client = MongoClient(MONGO_URI)
+client = MongoClient(
+    MONGO_URI,
+    tls=True,
+    tlsCAFile=certifi.where()
+)
 
 db = client["smart_retail_ai"]
 
